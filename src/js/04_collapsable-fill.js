709ebf78a47a4eb5e7af2626a8fd028f71423544
Barbara <barbara.estevez@gmail.com>
2021-05-11 00:02:49 +0200

    add teachers button profile file

--- src/js/04_collapsable-fill.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const collapsableSection = document.querySelector('.js-collapsable-header');
-
-collapsableSection.addEventListener('click',);
-function {
-
-}
---
+++ src/js/06_get-avatar.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fr = new FileReader();
+const fileField = document.querySelector('.js-inputImg');
+// const profileImage = document.querySelector('.js__profile-image');
+const profileImage = document.querySelector('.js-imgPreview');
+const profilePreview = document.querySelector('.js__profile-preview');
+
+
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que 
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e 
+ */
+function getImage(e){
+  const myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado. Ese resultado de procesar el fichero que hemos cargado
+   * podemos pasarlo como background a la imagen de perfil y a la vista previa
+   * de nuestro componente.
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
+function fakeFileClick() {
+ fileField.click(); 
+}
+
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+fileField.addEventListener('change', getImage);
+
+
+
+//------------------------------------------------//
+//---esto--es--lo--que--tenía--antes---//
+
+// const collapsableSection = document.querySelector('.js-collapsable-header');
+
+// collapsableSection.addEventListener('click',);
+// function {
+
+// }
