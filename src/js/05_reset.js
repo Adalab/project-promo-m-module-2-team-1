043c2d215dda67755ffc7ffeef1de4beb64cd3a0
Alba V <albavidalm@gmail.com>
2021-05-17 19:37:15 +0200

    add changed colors, to revise reset

--- src/js/05_reset.js
+++ src/js/05_reset.js
@@ -1,33 +1,28 @@
+function handleReset() {
+  formData.name = "Nombre Apellido";
+  formData.job = "Front-end developer";
+  formData.email = "";
+  formData.phone = "";
+  formData.linkedin = "";
+  formData.github = "";
+  formData.photo = "";
+  formData.palette = "opt1";
+  console.log(formData);
 
-function handleReset(){
+  namePreview.innerHTML = formData.name;
+  jobPreview.innerHTML = formData.job;
+  // imgPreview.style.backgroundImage = url('../images/avatar-provisional.png');
+  mailPreview.href = "mailto:";
+  telPreview.href = "tel:+34";
+  linkedinPreview.href = "https://www.linkedin.com/in/";
+  gitHubPreview.href = "https://github.com/";
 
-    formData.name = 'Nombre Apellido';
-    formData.job = 'Front-end developer';
-    formData.email = '';
-    formData.phone = '';
-    formData.linkedin = '';
-    formData.github = '';
-    formData.photo = '';
-    formData.palette = 'option1';
-    console.log(formData);
-        
-    namePreview.innerHTML = formData.name;
-    jobPreview.innerHTML = formData.job;
-    // imgPreview.style.backgroundImage = url('../images/avatar-provisional.png');
-    mailPreview.href = "mailto:";
-    telPreview.href = "tel:+34";
-    linkedinPreview.href = "https://www.linkedin.com/in/";
-    gitHubPreview.href = "https://github.com/";
-
-    inputName.value = '';
-    inputJob.value = '';
-    inputPhone.value = '';
-    inputEmail.value = '';
-    inputGitHub.value = '';
-    inputLinkedin.value = '';
-    
-    
-
+  inputName.value = "";
+  inputJob.value = "";
+  inputPhone.value = "";
+  inputEmail.value = "";
+  inputGitHub.value = "";
+  inputLinkedin.value = "";
 }
 
 resetButton.addEventListener("click", handleReset);
