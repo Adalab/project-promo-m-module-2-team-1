5274bdee554cfe3c896b94a0d11c6213f1d8f043
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-17 17:56:46 +0200

    add improvements handlerColor function

--- src/js/02_form-interactive.js
+++ src/js/02_form-interactive.js
@@ -2,57 +2,20 @@ function handlerColor(){
 
   const checkedRadio = document.querySelector('.js-colorOption:checked');
   const inputValue = checkedRadio.value;
-  console.log(inputValue);
 
-  const cardElement = document.querySelector('.page__preview--card')
-
-  borderPreview.classList.remove('js-op1__wrapper', 'js-op2__wrapper', 'js-op3__wrapper');
-  nameColorPreview.classList.remove('js-op1__name', 'js-op2__name', 'js-op3__name');
-  circlePreview1.classList.remove('js-op1__circle', 'js-op2__circle', 'js-op3__circle');
-  circlePreview2.classList.remove('js-op1__circle', 'js-op2__circle', 'js-op3__circle');
-  circlePreview3.classList.remove('js-op1__circle', 'js-op2__circle', 'js-op3__circle');
-  circlePreview4.classList.remove('js-op1__circle', 'js-op2__circle', 'js-op3__circle');
-  iconPreview1.classList.remove('js-op1__icon', 'js-op2__icon', 'js-op3__icon');
-  iconPreview2.classList.remove('js-op1__icon', 'js-op2__icon', 'js-op3__icon');
-  iconPreview3.classList.remove('js-op1__icon', 'js-op2__icon', 'js-op3__icon');
-  iconPreview4.classList.remove('js-op1__icon', 'js-op2__icon', 'js-op3__icon');
+  const cardElement = document.querySelector('.js-cardElement');
   
+  cardElement.classList.remove('opt1', 'opt2', 'opt3');
+
   
   if(inputValue === '1'){
-    borderPreview.classList.add('js-op1__wrapper');
-    nameColorPreview.classList.add('js-op1__name');
-    circlePreview1.classList.add('js-op1__circle');
-    circlePreview2.classList.add('js-op1__circle');
-    circlePreview3.classList.add('js-op1__circle');
-    circlePreview4.classList.add('js-op1__circle');
-    iconPreview1.classList.add('js-op1__icon');
-    iconPreview2.classList.add('js-op1__icon');
-    iconPreview3.classList.add('js-op1__icon');
-    iconPreview4.classList.add('js-op1__icon');
+    cardElement.classList.add('opt1');
+    
   }else if (inputValue === '2'){
-    //cardElement.classList.add('opt2');
-    borderPreview.classList.add('js-op2__wrapper');
-    nameColorPreview.classList.add('js-op2__name');
-    circlePreview1.classList.add('js-op2__circle');
-    circlePreview2.classList.add('js-op2__circle');
-    circlePreview3.classList.add('js-op2__circle');
-    circlePreview4.classList.add('js-op2__circle');
-    iconPreview1.classList.add('js-op2__icon');
-    iconPreview2.classList.add('js-op2__icon');
-    iconPreview3.classList.add('js-op2__icon');
-    iconPreview4.classList.add('js-op2__icon');
+    cardElement.classList.add('opt2');
+    
   }else if (inputValue === '3'){
-    borderPreview.classList.add('js-op3__wrapper');
-    nameColorPreview.classList.add('js-op3__name');
-    circlePreview1.classList.add('js-op3__circle');
-    circlePreview2.classList.add('js-op3__circle');
-    circlePreview3.classList.add('js-op3__circle');
-    circlePreview4.classList.add('js-op3__circle');
-    iconPreview1.classList.add('js-op3__icon');
-    iconPreview2.classList.add('js-op3__icon');
-    iconPreview3.classList.add('js-op3__icon');
-    iconPreview4.classList.add('js-op3__icon');
-
+    cardElement.classList.add('opt3');
   }
 }
 
