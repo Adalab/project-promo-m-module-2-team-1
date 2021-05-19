838fb3ea604e1bbde1ff7059780803f4f4ee2472
Lorellana21 <lorellana.ordonez@gmail.com>
2021-05-19 20:09:40 +0200

    add changes

--- src/js/07_localStorage.js
+++ src/js/07_localStorage.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable strict */
-function setLocalStorage(){
-  localStorage.setItem('formData', JSON.stringify(formData));
+function setLocalStorage() {
+  localStorage.setItem("formData", JSON.stringify(formData));
 }
-
